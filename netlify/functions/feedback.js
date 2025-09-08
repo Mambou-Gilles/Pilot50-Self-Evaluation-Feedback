2ada6bc7beffd8c9b05e4c6c64f92f06f8dc0fd5
Mambou-Gilles <gillesravelmambou@gmail.com>
2025-09-08 11:39:12 +0200

    Added package to netlify/functions

--- netlify/functions/feedback.js
+++ netlify/functions/feedback.js
@@ -1,21 +1,22 @@
+// /netlify/functions/feedback.js
 const fetch = require('node-fetch');
 
-exports.handler = async (event, context) => {
+exports.handler = async (event) => {
   try {
     const { cohort } = event.queryStringParameters;
-
+    
     if (!cohort) {
       return {
         statusCode: 400,
         body: JSON.stringify({ error: 'Missing required parameters.' }),
       };
     }
-
+    
     const url = `${process.env.GOOGLE_SCRIPT_URL}?cohort=${encodeURIComponent(cohort)}`;
-
+    
     const response = await fetch(url);
     const data = await response.json();
-
+    
     return {
       statusCode: 200,
       body: JSON.stringify(data),
